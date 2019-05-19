4f1cc158e721689607dfdd4ab19af1fa6a09de12
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-05-19 12:03:56 -0700

    created Toyota class and overide model to Prius

---
+++ Cars/Toyota.h
@@ -0,0 +1,20 @@
+//
+//  Toyota.h
+//  Cars
+//
+//  Created by Ekam Singh Dhaliwal on 2019-05-19.
+//  Copyright © 2019 ekam-singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Car.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Toyota : Car
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Cars/Toyota.m
@@ -0,0 +1,23 @@
+//
+//  Toyota.m
+//  Cars
+//
+//  Created by Ekam Singh Dhaliwal on 2019-05-19.
+//  Copyright © 2019 ekam-singh. All rights reserved.
+//
+
+#import "Toyota.h"
+
+
+@implementation Toyota
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.model = @"Prius";
+    }
+    return self;
+}
+
+@end
